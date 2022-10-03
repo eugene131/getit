416bf5fc29c096e48360ca92f0e4c015e7d4e12a
eugene <dign106@gmail.com>
2022-10-03 18:58:52 +0900

    3차시 과제

--- src/chapter_05/CommentList.jsx
+++ src/chapter_05/CommentList.jsx
@@ -6,10 +6,12 @@ const comments = [
     name: '배유진',
     comment: '안녕하세요, 소통해요.',
   },
+
   {
     name: '유재석',
     comment: '리액트 재밌어요~',
   },
+
   {
     name: '강민경',
     comment: '저도 배워보고 싶어요',
--- src/index.js
+++ src/index.js
@@ -5,16 +5,35 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import CommentList from './chapter_05/CommentList';
 import Clock from './chapter_04/Clock';
-
+import NotificationList from './chapter_06/NotificationList';
+import Accommodate from './chapter_07/Accommodate';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//7ch
+root.render(
+  <React.StrictMode>
+    <Accommodate />
+  </React.StrictMode>
+)
+
+/*5챕터
+root.render(
+  <React.StrictMode>
+    <NotificationList />
+  </React.StrictMode>
+);*/
+
+/*4 챕터
 setInterval(() => {
   root.render(
     <React.StrictMode>
       <CommentList />
     </React.StrictMode>,
   );
-}, 1000);
+}, 1000);*/
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
